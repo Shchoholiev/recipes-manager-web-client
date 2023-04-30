0fe60f186e9de3e3dbee055d477780bc634d3900
DESKTOP-3QMVDGD\User <polina.pavvlenko@gmail.com>
2023-04-30 21:48:38 +0300

    Search page and search result pages template were created

---
+++ RecipesManagerWebClient.Web/Pages/Recipes/SearchResult.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecipesManagerWebClient.Web.Pages.Recipes
+{
+    public class SearchResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- RecipesManagerWebClient.Web/wwwroot/js/site.js
+++ RecipesManagerWebClient.Web/wwwroot/js/site.js
@@ -2,3 +2,53 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const menuBox = document.querySelector(".menu-box");
+const burger = document.querySelector(".burger");
+const filterButtons = document.querySelectorAll(".filter-btn");
+const filter = document.querySelector(".filter");
+const filterBar = document.querySelector(".filter-bar");
+const applyFilter = document.querySelector(".apply-filter");
+
+function toggleMenu() {
+    if (menuBox.classList.contains("showMenu")) {
+        menuBox.classList.remove("showMenu");
+    } else {
+        menuBox.classList.add("showMenu");
+    }
+}
+
+burger.addEventListener("click", toggleMenu);
+
+
+filter.addEventListener("click", () => {
+    if (filterBar.style.display === "none" || filterBar.style.display === "") {
+        filterBar.style.opacity = "0";
+        filterBar.style.display = "block";
+        setTimeout(() => {
+            filterBar.style.opacity = "1";
+        }, 10);
+    } else {
+        filterBar.style.opacity = "0";
+        setTimeout(() => {
+            filterBar.style.display = "none";
+        }, 500);
+    }
+});
+
+function activateFilterButton(event) {
+    const clickedButton = event.target;
+    const filterButtons = document.querySelectorAll(".filter-btn");
+
+    filterButtons.forEach((button) => {
+        button.classList.remove("filter-active");
+    });
+
+    clickedButton.classList.add("filter-active");
+}
+
+
+filterButtons.forEach((button) => {
+    button.addEventListener("click", activateFilterButton);
+
+})
