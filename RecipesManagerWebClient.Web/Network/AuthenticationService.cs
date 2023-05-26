6deb8bfd24549ad8954de37cb3a47a6b5292ceb8
Mykhailo <mihail.beloded.work@gmail.com>
2023-05-26 19:06:45 +0300

    RM-90 all fixed

--- RecipesManagerWebClient.Web/Network/AuthenticationService.cs
+++ RecipesManagerWebClient.Web/Network/AuthenticationService.cs
@@ -25,39 +25,17 @@ public class AuthenticationService
     {
         var jwtToken = _httpContext.Request.Cookies["accessToken"];
 
-        if (string.IsNullOrEmpty(jwtToken) || IsJwtTokenExpired(jwtToken)) {
-            var webId = _httpContext.Request.Cookies["webId"];
-            if (string.IsNullOrEmpty(webId))
-            {
-                webId = Guid.NewGuid().ToString();
-                _httpContext.Response.Cookies.Append("webId", webId, new CookieOptions { Expires = DateTime.UtcNow.AddDays(180) });
-            }
-
-            var request = new GraphQLRequest
-            {
-                Query = @"
-                    mutation AccessWebGuest($user: AccessWebGuestModelInput!) {
-                        accessWebGuest(model: $user) {
-                            accessToken
-                            refreshToken
-                        }
-                    }",
-                Variables = new { user = new { webId = webId } } 
-            };
-
-            var response = await _graphQLClient.SendMutationAsync<dynamic>(request);
-            var jsonResponse = JsonConvert.SerializeObject(response.Data.accessWebGuest);
-            var tokens = JsonConvert.DeserializeObject<TokensModel>(jsonResponse);
-            _httpContext.Response.Cookies.Append("accessToken", tokens.AccessToken, new CookieOptions { Expires = DateTime.UtcNow.AddDays(180) });
-            _httpContext.Response.Cookies.Append("refreshToken", tokens.RefreshToken, new CookieOptions { Expires = DateTime.UtcNow.AddDays(180) });
-
-            jwtToken = tokens.AccessToken;
+        if (string.IsNullOrEmpty(jwtToken)) {
+            jwtToken = await AccessWebGuest();
         }
-
+        if (IsJwtTokenExpired(jwtToken))
+        {
+            jwtToken = await RefreshToken();
+        }
         return jwtToken;
     }
 
-    private bool IsJwtTokenExpired(string jwtToken)
+    public bool IsJwtTokenExpired(string jwtToken)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
 
@@ -77,5 +55,103 @@ public class AuthenticationService
         var roles = roleClaims.Select(c => c.Value).ToList();
 
         return roles;
+    }
+
+    public string? GetEmailFromJwtToken(string jwtToken)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.ReadJwtToken(jwtToken);
+        var claims = token.Claims;
+        var email = claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+        return email;
+    }
+
+    public string? GetNameFromJwtToken(string jwtToken)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.ReadJwtToken(jwtToken);
+        var claims = token.Claims;
+        var name = claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+
+        return name;
+    }
+
+    public string? GetPhoneFromJwtToken(string jwtToken)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.ReadJwtToken(jwtToken);
+        var claims = token.Claims;
+        var phone = claims.FirstOrDefault(c => c.Type == ClaimTypes.MobilePhone)?.Value;
+
+        return phone;
+    }
+
+    public string? GetIdFromJwtToken(string jwtToken)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.ReadJwtToken(jwtToken);
+        var claims = token.Claims;
+        var id = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+        return id;
+    }
+
+    private async Task<string> AccessWebGuest()
+    {
+        var webId = _httpContext.Request.Cookies["webId"];
+        if (string.IsNullOrEmpty(webId))
+        {
+            webId = Guid.NewGuid().ToString();
+            _httpContext.Response.Cookies.Append("webId", webId, new CookieOptions { Expires = DateTime.UtcNow.AddDays(180) });
+        }
+
+        var request = new GraphQLRequest
+        {
+            Query = @"
+                    mutation AccessWebGuest($user: AccessWebGuestModelInput!) {
+                        accessWebGuest(model: $user) {
+                            accessToken
+                            refreshToken
+                        }
+                    }",
+            Variables = new { user = new { webId = webId } }
+        };
+
+        var response = await _graphQLClient.SendMutationAsync<dynamic>(request);
+        var jsonResponse = JsonConvert.SerializeObject(response.Data.accessWebGuest);
+        var tokens = JsonConvert.DeserializeObject<TokensModel>(jsonResponse);
+        _httpContext.Response.Cookies.Append("accessToken", tokens.AccessToken, new CookieOptions { Expires = DateTime.UtcNow.AddDays(180) });
+        _httpContext.Response.Cookies.Append("refreshToken", tokens.RefreshToken, new CookieOptions { Expires = DateTime.UtcNow.AddDays(180) });
+
+        return tokens.AccessToken;
+    }
+
+    private async Task<string> RefreshToken()
+    {
+        var accessToken = _httpContext.Request.Cookies["accessToken"];
+        var refreshToken = _httpContext.Request.Cookies["refreshToken"];
+
+        var request = new GraphQLRequest
+        {
+            Query = @"
+                    mutation RefreshUserToken($model: TokensModelInput!) {
+                        refreshUserToken(model: $model) {
+                            accessToken
+                            refreshToken
+                        }
+                    }",
+            Variables = new { model = new { accessToken = accessToken, refreshToken = refreshToken } }
+        };
+
+        var response = await _graphQLClient.SendMutationAsync<dynamic>(request);
+        var jsonResponse = JsonConvert.SerializeObject(response.Data.refreshUserToken);
+        var tokens = JsonConvert.DeserializeObject<TokensModel>(jsonResponse);
+        _httpContext.Response.Cookies.Delete("accessToken");
+        _httpContext.Response.Cookies.Delete("refreshToken");
+        _httpContext.Response.Cookies.Append("accessToken", tokens.AccessToken, new CookieOptions { Expires = DateTime.UtcNow.AddDays(180) });
+        _httpContext.Response.Cookies.Append("refreshToken", tokens.RefreshToken, new CookieOptions { Expires = DateTime.UtcNow.AddDays(180) });
+
+        return tokens.AccessToken;
     }
 }
---
+++ RecipesManagerWebClient.Web/СustomMiddlewares/GlobalUserMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using RecipesManagerWebClient.Web.Models.GlobalInstances;
+using RecipesManagerWebClient.Web.Network;
+using System.Security.Claims;
+
+namespace RecipesManagerWebClient.Web.СustomMiddlewares;
+
+public class GlobalUserMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalUserMiddleware(RequestDelegate next)
+    {
+        this._next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext, AuthenticationService authenticationService)
+    {
+        var accessToken = await authenticationService.GetAuthTokenAsync();
+
+        if (!string.IsNullOrEmpty(accessToken))
+        {
+            GlobalUser.Roles = authenticationService.GetRolesFromJwtToken(accessToken);
+            GlobalUser.Id = authenticationService.GetIdFromJwtToken(accessToken);
+            GlobalUser.Email = authenticationService.GetEmailFromJwtToken(accessToken);
+            GlobalUser.Phone = authenticationService.GetPhoneFromJwtToken(accessToken);
+            GlobalUser.Name = authenticationService.GetNameFromJwtToken(accessToken);
+        }
+
+        await _next(httpContext);
+    }
+}
