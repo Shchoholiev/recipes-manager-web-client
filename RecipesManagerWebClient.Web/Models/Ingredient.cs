f795efbe7c92a93f50abdf309a64e53cbc160af5
Yelyzaveta Boiko <yelyzaveta.boiko1@nure.ua>
2023-05-29 22:31:12 +0200

    RM-108-shopping-lists was developed

---
+++ RecipesManagerWebClient.Web/Models/Ingredient.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipesManagerWebClient.Web.Models
+{
+    public class Ingredient
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Units { get; set; }
+        public float Amount { get; set; }
+        public int CaloriesPerUnit { get; set; }
+        public int TotalCalories { get; set; }
+    }
+}
---
+++ RecipesManagerWebClient.Web/Models/ShoppingList.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipesManagerWebClient.Web.Models
+{
+    public class ShoppingList
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public List<Recipe> Recipes { get; set; }
+        public string Notes { get; set; }
+        public List<User> SentTo { get; set; }
+    }
+}
